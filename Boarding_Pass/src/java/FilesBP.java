7dba8823327ec1de62a4525427d94f30028905b5
Vi Nguyen <fluffywin@gmail.com>
2022-02-04 15:50:08 -0500

    implementing manage menu

--- Boarding_Pass/src/java/FilesBP.java
+++ Boarding_Pass/src/java/FilesBP.java
@@ -8,6 +8,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Used to read and write to text file that is located in the resource folder
@@ -19,14 +20,35 @@ public class FilesBP {
     static Charset utf8 = StandardCharsets.UTF_8;
     static Path path = Paths.get("src/resources/boarding_pass_data.txt");
     static ArrayList<Integer> bpID = new ArrayList<>();
+    static ArrayList<BoardingPass> allBP = new ArrayList<>();
 
     // needs all id first
     public FilesBP() {
         allID();
+        allBPsetup();
+    }
+
+    public static void allBPsetup(){
+        readFromFile()
+                .stream()
+                .map( e -> Arrays.stream(e.split(","))
+                        .map(str -> str.split("'")[1])
+                        .collect( Collectors.toList() ) )
+                .forEach(bp -> {
+                    allBP.add( new BoardingPass(
+                            bp.get(0), bp.get(1), bp.get(2), bp.get(3),
+                            Integer.parseInt(bp.get(4)), bp.get(5), bp.get(6), bp.get(7),
+                            Integer.parseInt(bp.get(8)), bp.get(9), bp.get(10), bp.get(11) )
+                    );
+                });
     }
 
     // find the id within the list
     public static void allID(){
+        if(readFromFile().isEmpty()) {
+            bpID.add(0);
+            return;
+        }
         for(String boardingPass : readFromFile() ) {
             bpID.add( Integer.parseInt( boardingPass.split("'")[17] ) );
         }
@@ -62,5 +84,13 @@ public class FilesBP {
         }
 
     }
+
+    public static void main(String[] args) {
+        FilesBP file = new FilesBP();
+
+        allBP.stream().forEach(a -> System.out.println(a.getEmail()));
+    }
+
+
 
 }
--- Boarding_Pass/src/test/BoardingPassTest.java
+++ Boarding_Pass/src/test/BoardingPassTest.java
@@ -10,8 +10,8 @@ class BoardingPassTest {
     @BeforeEach
     void setUp(){
         testPass = new BoardingPass("Jimmy",
-                "Fluffy", "404", "m", 21,
-                "now", "here", "there", "now");
+                "Test@gmail.com", "404", "m", 21,
+                "now", "Georgia", "California", "now");
     }
 
     @Test
@@ -25,15 +25,125 @@ class BoardingPassTest {
         assertEquals("Joey", testPass.getName(), "Error: setName failed");
     }
 
+    @Test
+    public void getEmailTest(){
+        assertEquals("Test@gmail.com", testPass.getEmail(), "Error: getEmail failed");
+    }
 
     @Test
-    public void calculatePriceTest(){
-        assertEquals("20.0", testPass.calculatePrice(), "Error: calculatePrice failed");
+    public void setEmailTest(){
+        testPass.setEmail("TestTest@tester.net");
+        assertEquals("TestTest@tester.net", testPass.getEmail(), "Error: getEmail failed");
+    }
+
+    @Test
+    public void getPhoneTest(){
+
+    }
+
+    @Test
+    public void setPhoneTest(){
+
+    }
+
+    @Test
+    public void getGenderTest(){
+        assertEquals("m", testPass.getGender(), "Error: getGender failed");
+    }
+
+    @Test
+    public void setGenderTest(){
+
+    }
+
+    @Test
+    public void getAgeTest(){
+
+    }
+
+    @Test
+    public void setAgeTest(){
+
+    }
+
+    @Test
+    public void getDateTest(){
+
+    }
+
+    @Test
+    public void setDateTest(){
+
+    }
+
+    @Test
+    public void getDestinationTest(){
+
+    }
+
+    @Test
+    public void setDestinationTest(){
+
+    }
+
+    @Test
+    public void getDepartureTimeTest(){
+
+    }
+
+    @Test
+    public void setDepartureTimeTest(){
+
+    }
+
+    @Test
+    public void getOriginTest(){
+
+    }
+
+    @Test
+    public void setOriginTest(){
+
+    }
+
+    @Test
+    public void getEtaTest(){
+
+    }
+
+    @Test
+    public void setEtaTest(){
+
+    }
+
+    @Test
+    public void getPriceTest(){
+
+    }
+
+    @Test
+    public void setPriceTest(){
+
     }
 
     @Test
     public void generateBPNumberTest(){
+        assertEquals(true,
+                !( new FilesBP().bpID.contains( testPass.generateBPNumber() ) ),
+                "Error: generateBPNumber failed" );
+    }
 
+    @Test
+    public void calculatePriceTest(){
+        testPass.setGender("female");
+        assertEquals("75.0", testPass.calculatePrice(100),"Error: calculatePrice failed");
+    }
+
+    @Test
+    public void generateETATest(){
+        assertEquals("4 hours, 37 minutes",
+                testPass.generateETA("Georgia","California"),
+                "Error: generateETA failed");
     }
 
 
